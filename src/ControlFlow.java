19487cf964e9be39d9edd5908f8f909364d8fcba
seksatta_HP <seksattha@gmail.com>
2024-07-16 11:47:29 +0700

    2 switch

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -9,5 +9,26 @@ public class ControlFlow {
         }else {
             System.out.println("You're too old!");
         }
+
+        int dayOfWeek = 3;
+        String dayName;
+
+        switch (dayOfWeek) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+
+        System.out.println("Day of the week is: " + dayName);
+
     }
 }
