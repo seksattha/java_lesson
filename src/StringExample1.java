ced06ec5c7b760d2c8fc52c9cea9e8b958c83750
seksatta_HP <seksattha@gmail.com>
2024-07-16 10:35:26 +0700

    1

---
+++ src/StringExample1.java
@@ -0,0 +1,10 @@
+public class StringExample1{
+    public static void  main (String[] args){
+        System.out.println("String tutorial");
+        String a = "Dream";
+        String b = "Theater";
+        //ระวังให้ดีตอนประกาศตัวแปรต้องเป็นตัวใหญ่
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
